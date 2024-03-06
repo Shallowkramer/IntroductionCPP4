4fb26a85f815871810c0b29e7bdab9b853a2bae9
Shallowkramer <mark_richards@outlook.com.au>
2024-03-06 19:48:35 +1030

    fixed build errors with gamestatess

--- IntroCPPTask4TextAdventure/GameStates.h
+++ IntroCPPTask4TextAdventure/GameStates.h
@@ -13,9 +13,9 @@ enum class EGameState
 class GameStates
 {
 public:
-	virtual void Load(); //loads anything needed for the game state
-	virtual void Update(); //Updates anything needed in the game state
-	virtual void Draw(); //draws anything needed for the game state
-	virtual void Unload(); //Will call deconstructor of any gamestates which will unload that gamestate
+	virtual void Load() =0; //loads anything needed for the game state
+	virtual void Update() =0; //Updates anything needed in the game state
+	virtual void Draw()=0; //draws anything needed for the game state
+	virtual void Unload()=0; //Will call deconstructor of any gamestates which will unload that gamestate
 };
 
--- IntroCPPTask4TextAdventure/PlayState.cpp
+++ IntroCPPTask4TextAdventure/PlayState.cpp
@@ -12,20 +12,20 @@ PlayState::~PlayState()
 
 void PlayState::Load()
 {
-
+	return;
 }
 
 void PlayState::Update()
 {
-
+	return;
 }
 
 void PlayState::Draw()
 {
-
+	return;
 }
 
 void PlayState::Unload()
 {
-
+	return;
 }
